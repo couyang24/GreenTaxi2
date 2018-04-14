a2a581ad644719a96ba800e37b8693c2ef11f06e
Chengran Ouyang <30837746+couyang24@users.noreply.github.com>
2018-04-14 15:04:01 -0400

    question2

--- main.R
+++ main.R
@@ -1,6 +1,6 @@
 # Setup packages and load data
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(tidyverse, DT, lubridate, leaflet, leaflet.extras, maps, data.table)
+pacman::p_load(tidyverse, DT, lubridate, leaflet, leaflet.extras, maps, data.table, ggthemes)
 # data <- read.csv("green_tripdata_2016-02v2.csv", stringsAsFactors = F)
 # write_csv(data,"green_tripdata_2016-02v2.csv")
 data <- fread("green_tripdata_2016-02v2.csv", stringsAsFactors = F, data.table = FALSE, na.strings=c("NA","NaN","?", ""))
@@ -22,13 +22,34 @@ colSums(is.na(data))
 # Presumably 
 data$VendorID %>% table() 
 
-data %>% mutate(lpep_pickup_datetime = ymd_hms(lpep_pickup_datetime),
-                pickup_hour=hour(lpep_pickup_datetime),
-                pickup_weekday=weekdays(lpep_pickup_datetime),
-                Lpep_dropoff_datetime = ymd_hms( Lpep_dropoff_datetime)) %>% 
-  head()
+
+data %>%
+  ggplot(aes(Trip_distance)) +
+  geom_histogram(fill = "firebrick", bins = 150) +
+  scale_x_log10() +
+  scale_y_sqrt()
 
 
+data <- data %>% mutate(lpep_pickup_datetime = ymd_hms(lpep_pickup_datetime),
+                pickup_hour=hour(lpep_pickup_datetime)+1,
+                pickup_weekday=as.factor(weekdays(lpep_pickup_datetime)),
+                pickup_weekend=if_else(pickup_weekday=='Saturday'|pickup_weekday=='Sunday','Weekend','Weekday'),
+                Lpep_dropoff_datetime = ymd_hms( Lpep_dropoff_datetime),
+                dropoff_hour=hour(lpep_pickup_datetime)+1,
+                dropoff_weekday=as.factor(weekdays(lpep_pickup_datetime)))
+
+
+
+
+data %>% group_by(pickup_hour) %>% summarise(avg_trip_distance=mean(Trip_distance)) %>% 
+  ggplot(aes(pickup_hour, avg_trip_distance)) + geom_col() +
+  geom_label(aes(label=round(avg_trip_distance,1)), size=3.5, alpha=.7) +
+  # coord_flip() +
+  scale_x_continuous(breaks=seq(1,24,1)) +
+  theme_economist() +
+  theme(legend.position = 'none') +
+  labs(title='Gale Interview Challenge',subtitle='by Owen Ouyang',caption="source: Green Taxi Data",
+       y="Average Trip Distance", x="Time of Day (Pickup)")
 
 
 data$Store_and_fwd_flag %>% table()
